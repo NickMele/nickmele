a8c71ac343332324ca7e0d059f42f4cd09a17fb4
Nick Mele <nickmele26@gmail.com>
2013-04-30 23:38:14 -0400

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Nick Mele</title>
+
+	<!-- Mobile Optimization (responsive sites only) http://goo.gl/n11kQ -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+
+	<link rel="stylesheet" href="assets/css/nickmele.css">
+
+</head>
+<body>
+
+	<header>
+		<div class="row">
+			<div class="logo">
+				<a href="">Nick Mele</a>
+			</div>
+			<nav>
+				<ul>
+					<li class="nav-active"><a href="" data-icon="&#xe001;">Home</a></li>
+					<li><a href="" data-icon="&#xe004;">Portfolio</a></li>
+					<li><a href="" data-icon="&#xe005;">Connect</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<div class="main-content">
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once("includes/header.php"); ?>
+
+	<div class="row-full banner">
+		<div class="row">
+			<h1>Web Design & Development</h1>
+			<p>with a touch of awesomeness</p>
+		</div>
+	</div>
+
+	
+
+<?php require_once("includes/footer.php"); ?>
